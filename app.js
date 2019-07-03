a50ad0d4d90409c2486708b07c871c384148b188
Jack <jacksondearing96@.gmail.com>
2019-07-03 22:58:25 +0930

    added simple server

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+
+var indexRouter = require('./routes/index');
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+
+app.listen(3000);
+
+module.exports = app;
